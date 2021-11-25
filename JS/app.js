b637277506adffdce2aea1722f047d657e15e161
Kush <jaiswalkush8@gmail.com>
2021-11-25 10:19:54 +0530

    Added song play

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,15 @@
 console.log('Kush Jaiswal');
 let songs = [
-    {songName:"SLOW DANCING IN THE DARK", filePath:"/Assets/songs/SLOW DANCING IN THE DARK.mp3",coverPath:"/Assets/imgs/Joji_Ballads_1.png",artistName:"Joji"}
+    {songName:"SLOW DANCING IN THE DARK", filePath:"/Assets/songs/Song0.mp3",coverPath:"/Assets/imgs/Joji_Ballads_1.png",artistName:"Joji"},
+    {songName:"Sanctuary", filePath:"/Assets/songs/Song1.mp3",coverPath:"/Assets/imgs/Joji-Nectar.png",artistName:"Joji"},
+    {songName:"SAD!", filePath:"/Assets/songs/Song2.mp3",coverPath:"/Assets/imgs/xxxtentation-cover.jpeg",artistName:"XXXTENTATION"},
+    {songName:"Marijiuana Breath", filePath:"/Assets/songs/Song3.mp3",coverPath:"/Assets/imgs/marijuana-breath.jpg",artistName:"Adam Jensen"},
+    {songName:"STAY", filePath:"/Assets/songs/Song4.mp3",coverPath:"/Assets/imgs/Stay-justin-bieber.jpg",artistName:"The Kid LOROI,Justin Bieber"},
+    {songName:"Let Me Love You", filePath:"/Assets/songs/Song5.mp3",coverPath:"/Assets/imgs/let-me-love-you.jpeg",artistName:"The Kid LOROI,Justin Bieber"},
 ]
 
 let songIndex = 0;
-let audioElement = new Audio('/Assets/songs/SLOW DANCING IN THE DARK.mp3');
+let audioElement = new Audio('/Assets/song/song0.mp3')
 let masterPlay = document.querySelector('#masterPlay');
 let pauseIcon = document.querySelector('#pauseIcon');
 let playIcon = document.querySelector('#playIcon');
@@ -31,4 +36,15 @@ audioElement.addEventListener('timeupdate',()=>{
 
 myProgressBar.addEventListener('change' ,()=>{
     audioElement.currentTime = myProgressBar.value * audioElement.duration /100;
+})
+
+Array.from(document.getElementsByClassName("song-box")).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        index = parseInt(e.target.id);
+        audioElement.src = "/Assets/songs/Song${song}.mp3";     
+        audioElement.currentTime = 0;
+        audioElement.play();
+        pauseIcon.style.display ="block";
+        playIcon.style.display = "none";
+    })
 })
