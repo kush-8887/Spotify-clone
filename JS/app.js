a8bc8a2f6f1456df759a8e7c0fb49c6fb32f51f0
Kush <jaiswalkush8@gmail.com>
2021-11-26 11:31:38 +0530

    Fixed minor bugs

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,22 @@
 console.log('Kush Jaiswal');
 let songs = [
-    {songName:"SLOW DANCING IN THE DARK", filePath:"/Assets/songs/Song0.mp3",coverPath:"/Assets/imgs/Joji_Ballads_1.png",artistName:"Joji"},
-    {songName:"Sanctuary", filePath:"/Assets/songs/Song1.mp3",coverPath:"/Assets/imgs/Joji-Nectar.png",artistName:"Joji"},
-    {songName:"SAD!", filePath:"/Assets/songs/Song2.mp3",coverPath:"/Assets/imgs/xxxtentation-cover.jpeg",artistName:"XXXTENTATION"},
-    {songName:"Marijiuana Breath", filePath:"/Assets/songs/Song3.mp3",coverPath:"/Assets/imgs/marijuana-breath.jpg",artistName:"Adam Jensen"},
-    {songName:"STAY", filePath:"/Assets/songs/Song4.mp3",coverPath:"/Assets/imgs/Stay-justin-bieber.jpg",artistName:"The Kid LOROI,Justin Bieber"},
-    {songName:"Let Me Love You", filePath:"/Assets/songs/Song5.mp3",coverPath:"/Assets/imgs/let-me-love-you.jpeg",artistName:"The Kid LOROI,Justin Bieber"},
+    {songName:"SLOW DANCING IN THE DARK", filePath:"/Assets/songs/Song0.mp3",coverPath:"/Assets/imgs/Img0.jpg",artistName:"Joji"},
+    {songName:"Sanctuary", filePath:"/Assets/songs/Song1.mp3",coverPath:"/Assets/imgs/Img1.jpg",artistName:"Joji"},
+    {songName:"SAD!", filePath:"/Assets/songs/Song2.mp3",coverPath:"/Assets/imgs/Img2.jpg",artistName:"XXXTENTATION"},
+    {songName:"Marijiuana Breath", filePath:"/Assets/songs/Song3.mp3",coverPath:"/Assets/imgs/Img3.jpg",artistName:"Adam Jensen"},
+    {songName:"STAY", filePath:"/Assets/songs/Song4.mp3",coverPath:"/Assets/imgs/Img4.jpg",artistName:"The Kid LOROI,Justin Bieber"},
+    {songName:"Let Me Love You", filePath:"/Assets/songs/Song5.mp3",coverPath:"/Assets/imgs/Img5.jpeg",artistName:"The Kid LOROI,Justin Bieber"},
 ]
 
 let songIndex = 0;
-let audioElement = new Audio('/Assets/song/song0.mp3')
+let imageIndex = 0;
+let audioElement = new Audio("");
 let masterPlay = document.querySelector('#masterPlay');
 let pauseIcon = document.querySelector('#pauseIcon');
 let playIcon = document.querySelector('#playIcon');
 let myProgressBar = document.querySelector('#song-progress-bar');
+let currentSongImg = document.querySelector('#currentSongImgChange');
+let currentSongName = document.querySelector('#currentSongName');
 
 //Handle play/pause 
 masterPlay.addEventListener('click',()=>{
@@ -40,11 +43,13 @@ myProgressBar.addEventListener('change' ,()=>{
 
 Array.from(document.getElementsByClassName("song-box")).forEach((element)=>{
     element.addEventListener('click',(e)=>{
-        index = parseInt(e.target.id);
-        audioElement.src = "/Assets/songs/Song${song}.mp3";     
+        songIndex = parseInt(e.target.id);
+        imageIndex = parseInt(e.target.id)
+        currentSongImg.src = `/Assets/imgs/Img${imageIndex}.jpg`
+        audioElement.src = `/Assets/songs/Song${songIndex}.mp3`;
         audioElement.currentTime = 0;
-        audioElement.play();
         pauseIcon.style.display ="block";
         playIcon.style.display = "none";
+        audioElement.play();
     })
 })
